b1fcbc8075955ca5213bcd214b57fad9d8f1df82
Samanta <slaragiannone@gmail.com>
2018-11-06 16:50:52 +0000

    cambios

---
+++ database/seeds/user_seeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class user_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        for ($i = 0; $i < 10; $i++)
+            static::Create_User ((object)[
+                'MAIL' => 'tutu' , $i . '@adpcprojects.com',
+                'NAME' => 'tutu' . $i,
+                'PASSWORD' => 'tutu' . $i
+            ]);
+    }
+
+    private static function Create_User ($objUser) {
+        $uUser = new User;
+        $uUser -> email = $objUser -> MAIL;
+        $uUser -> name = $objUser -> NAME;
+        $uUser -> password = Hash::make($objUser -> PASSWORD);
+
+        $uUser -> save();
+
+        echo ' - ' . $uUser -> id . "\n";
+    }
+}
